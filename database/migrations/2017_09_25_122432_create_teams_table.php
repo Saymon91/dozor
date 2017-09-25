555aebdf2bef43a55639c5f2151640006fdc2053
semen <semen.zjuravlev@gmail.com>
2017-09-25 19:02:51 +0300

    first commit

---
+++ database/migrations/2017_09_25_122432_create_teams_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->timestamps();
+            $table->string('name', 25);
+            $table->json('info');
+            $table->primary('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+    }
+}
---
+++ database/migrations/2017_09_25_124004_create_regions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRegionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('regions', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->smallInteger('code');
+            $table->json('info');
+            $table->primary('code');
+            $table->unique(['id', 'code', 'name']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('regions');
+    }
+}
